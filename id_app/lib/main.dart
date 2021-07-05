bfbd06329b13857b7c5cb6cb856cec1b18c87fb7
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-06 01:07:44 +0700

    initial commit

---
+++ id_app/lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: id_card(),
+));
+
+class id_card extends StatefulWidget {
+  @override
+  _id_cardState createState() => _id_cardState();
+}
+
+class _id_cardState extends State<id_card> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        title: Text("Id Card"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[500],
+        elevation: 0,
+      ),
+
+      body: Padding(
+        
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/maxverstappen_avatar.jpg"),
+                radius: 40,
+              ),
+            ),
+
+            Divider(
+              height: 60,
+              color: Colors.grey[300]
+            ),
+   
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 18
+              ),
+            ),
+
+            SizedBox(
+              height: 10
+            ),
+
+            Text(
+              'Max Verstappen',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+            SizedBox(
+              height: 30
+            ),
+
+            Text(
+              'AGE (2021)',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 18
+              ),
+            ),
+
+            SizedBox(
+              height: 10
+            ),
+
+            Text(
+              '24 Years Old',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+            SizedBox(
+              height: 30
+            ),
+
+            Row(
+              children: <Widget> [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400]
+                ),
+
+                SizedBox(
+                  width: 10
+                ),
+
+                Text(
+                  "maxverstappen@gmail.com",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+          
+            SizedBox(
+              height: 25
+            ),
+
+            Center(
+              child: Image(
+              image: AssetImage("assets/redbullracinghonda_logo.png"),
+              height: 100,
+              width: 200
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
