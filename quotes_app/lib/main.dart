e6acd759f73f82d55d98633fb598ae314a64ee3c
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-06 02:01:29 +0700

    added appbar

--- quotes_app/lib/main.dart
+++ quotes_app/lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    home: quote_list()
+
+  ));
 }
 
+class quote_list extends StatefulWidget {
+  @override
+  _quote_listState createState() => _quote_listState();
+}
+
+class _quote_listState extends State<quote_list> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.blue[500],
+        title: Text("Quotes"),
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
