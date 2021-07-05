79cfb3898b502eb0eefb7bd145de7f09db7d9d62
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-06 02:10:42 +0700

    modified theme font

--- id_app/lib/main.dart
+++ id_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+  theme: ThemeData(fontFamily: 'Lato'),
   home: id_card(),
 ));
 
