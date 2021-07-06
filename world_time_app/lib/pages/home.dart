6c8c9c1f4a910e13e5f880723f7ed651c5bd4787
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-06 22:44:59 +0700

    added different pages

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text ('Home Screen')),
+    );
+  }
+}
