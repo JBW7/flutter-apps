aa204c97deb6dd45e7851faa6def097bbb995d8f
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-06 09:53:32 +0700

    added quotes

--- quotes_app/lib/main.dart
+++ quotes_app/lib/main.dart
@@ -13,6 +13,13 @@ class quote_list extends StatefulWidget {
 }
 
 class _quote_listState extends State<quote_list> {
+  
+  List <String> quotes = [
+    "It always seems impossible until it's done",
+    "Failure is not the opposite of success, it's a part of success",
+    "When something is important enough you do it even if the odds are not iy your favor",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +30,12 @@ class _quote_listState extends State<quote_list> {
         backgroundColor: Colors.blue[500],
         title: Text("Quotes"),
         centerTitle: true,
+      ),
+
+      body: Column(
+        children: <Widget>[
+
+        ],
       ),
       
     );
