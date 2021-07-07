50f5c9df8209c4b21d71593384e2869a2615e178
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-07 11:29:38 +0700

    added widgets

--- world_time_app/lib/pages/choose_location.dart
+++ world_time_app/lib/pages/choose_location.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class choose_location extends StatefulWidget { 
-  
+
   @override
   _choose_locationState createState() => _choose_locationState();
 }
@@ -10,6 +10,15 @@ class _choose_locationState extends State<choose_location> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.blue[500],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
 
       body: Text('Choose location'),
       
