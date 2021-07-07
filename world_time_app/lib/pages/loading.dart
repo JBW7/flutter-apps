56d5af96db188546593f71c294eb148e84c01e22
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-07 23:53:30 +0700

    extracting properties from api

--- world_time_app/lib/pages/loading.dart
+++ world_time_app/lib/pages/loading.dart
@@ -15,7 +15,16 @@ void get_data() async {
   // make the request to api
   Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/America/New_York'));
   Map data = jsonDecode(response.body);
-  print(data);
+
+  // get properties from data
+  String date_time = data ['datetime'];
+  String offset = data ['utc_offset'].substring(1, 3);
+
+  // convert date_time to object
+  DateTime now = DateTime.parse(date_time);
+  now = now.add(Duration(hours: int.parse(offset)));
+  print(now);
+
   }
 
   @override
