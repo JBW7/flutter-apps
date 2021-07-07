94b0e1cd3f35f698c7b46279a54b2efd41c3efa0
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-07 11:17:31 +0700

    add routes

--- world_time_app/lib/main.dart
+++ world_time_app/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: home(),
+
+    initialRoute: '/home',
+
+    routes: {
+      '/': (context) => loading(),
+      '/home': (context) => home(),
+      '/location': (context) => choose_location()
+    },
   ));
 }
 
