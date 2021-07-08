17bbcd75e70a49125c3874f8e0533312f59aa6b5
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-08 12:33:52 +0700

    day and night time bg

--- world_time_app/lib/pages/home.dart
+++ world_time_app/lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class home extends StatefulWidget {
 
   @override
@@ -16,51 +17,80 @@ class _homeState extends State<home> {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
 
+    // set background image (day or night)
+    String bg_image = data['is_day_time'] ? 'day.png' : 'night.png';
+    Color bg_color = data['is_day_time'] ? Colors.blue : Colors.indigo[700];
+    
     return Scaffold(
+      
+      backgroundColor: bg_color,
 
       body: SafeArea(
 
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: <Widget>[
-              TextButton.icon(
-                onPressed: () {
+        child: Container(
+          
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/$bg_image'),
+              fit: BoxFit.cover
+            )
+          ),
+          
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          
+            child: Column(
+          
+              children: <Widget>[
+          
+                TextButton.icon(
+                  onPressed: () {
 
-                  Navigator.pushNamed(context, '/location');
+                    Navigator.pushNamed(context, '/location');
 
-                },
-                
-                icon: Icon(Icons.edit_location),
-                label: Text ('Edit Location'),
-              ),
-              
-              SizedBox(
-                height: 20),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28,
-                      letterSpacing: 2,
+                  },
+                  
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300]
                     ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-
-              Text(
-                data ['time'],
-                style: TextStyle(
-                  fontSize: 66,
+                  label: Text (
+                    'Edit Location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                    ),
                 ),
-              )
-            ],
+                
+                SizedBox(
+                  height: 20),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28,
+                        letterSpacing: 2,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                Text(
+                  data ['time'],
+                  style: TextStyle(
+                    fontSize: 66,
+                    color: Colors.white
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
