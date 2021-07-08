d1f445ef7c988415af0a9975c889b9bf8c020435
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-08 11:14:35 +0700

    moved classes to world_time.dart

--- world_time_app/lib/pages/loading.dart
+++ world_time_app/lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time_app/services/world_time.dart';
+
 
 class loading extends StatefulWidget {
 
@@ -10,27 +10,21 @@ class loading extends StatefulWidget {
 
 class _loadingState extends State<loading> {
 
-void get_data() async {
+  String time = 'loading';
 
-  // make the request to api
-  Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/America/New_York'));
-  Map data = jsonDecode(response.body);
-
-  // get properties from data
-  String date_time = data ['datetime'];
-  String offset = data ['utc_offset'].substring(1, 3);
-
-  // convert date_time to object
-  DateTime now = DateTime.parse(date_time);
-  now = now.add(Duration(hours: int.parse(offset)));
-  print(now);
-
+  void setup_world_time() async {
+    world_time instance = world_time(location: 'New_York', flag: 'america.png', url: 'America/New_York');
+    await instance.get_time();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    get_data();
+    setup_world_time();
     
   }
 
@@ -38,7 +32,10 @@ void get_data() async {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+      ),
       
     );
   }
