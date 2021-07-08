40e6a5478c2cccebd748f6612830484b7738e327
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-08 13:28:51 +0700

    added list of world time

--- world_time_app/lib/pages/choose_location.dart
+++ world_time_app/lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
 
 class choose_location extends StatefulWidget { 
 
@@ -7,6 +8,17 @@ class choose_location extends StatefulWidget {
 }
 
 class _choose_locationState extends State<choose_location> {
+
+   List<world_time> locations = [
+	    world_time(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+	    world_time(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+	    world_time(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+	    world_time(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+	    world_time(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+	    world_time(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+	    world_time(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+	    world_time(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+	  ];
 
   @override
   Widget build(BuildContext context) {
