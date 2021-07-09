51d552dd8629614712458e88e8babb76b9c6c44b
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-09 16:19:24 +0700

    completed all operating buttons

--- simple_calculator_app/lib/main.dart
+++ simple_calculator_app/lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 // button dimensions
 double button_width = 100;
 double button_height = 100;
+double button_font_size = 20;
 
 
 class home extends StatefulWidget {
@@ -67,7 +68,7 @@ class _homeState extends State<home> {
 
 
           //buttons
-          // 2nd row
+          // 1st row
           Row(
 
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -91,6 +92,7 @@ class _homeState extends State<home> {
                       'C',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -116,6 +118,7 @@ class _homeState extends State<home> {
                       '±',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -140,6 +143,7 @@ class _homeState extends State<home> {
                       '%',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -147,7 +151,7 @@ class _homeState extends State<home> {
                 ),
               ),
               
-              // + button
+              // / button
               Padding(
                 padding: const EdgeInsets.fromLTRB(0, 10, 10, 0),
                 child: SizedBox(
@@ -162,9 +166,10 @@ class _homeState extends State<home> {
 
                     child:
                     Text(
-                      '+',
+                      '÷',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -174,7 +179,7 @@ class _homeState extends State<home> {
             ],
           ),
 
-          // 2st row
+          // 2nd row
           Row(
 
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -182,7 +187,7 @@ class _homeState extends State<home> {
             children: [
               // 7 button
               Padding(
-                padding: const EdgeInsets.fromLTRB(10, 5, 4, 10),
+                padding: const EdgeInsets.fromLTRB(10, 4, 4, 0),
                 child: SizedBox(
                   height: button_height,
                   width: button_width,
@@ -198,6 +203,7 @@ class _homeState extends State<home> {
                       '7',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -207,7 +213,7 @@ class _homeState extends State<home> {
 
               // 8 button
               Padding(
-                padding: const EdgeInsets.fromLTRB(0, 4, 0, 10),
+                padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
                 child: SizedBox(
                   height: button_height,
                   width: button_width,
@@ -223,6 +229,7 @@ class _homeState extends State<home> {
                       '8',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -232,7 +239,7 @@ class _homeState extends State<home> {
 
               // 9 button
               Padding(
-                padding: const EdgeInsets.fromLTRB(4, 4, 4, 10),
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
                 child: SizedBox(
                   height: button_height,
                   width: button_width,
@@ -247,6 +254,229 @@ class _homeState extends State<home> {
                       '9',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+              
+              // x button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 10, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      'x',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // 3rd row
+          Row(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              // 4 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '4',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // 5 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '5',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // 6 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '6',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+              
+              // - button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 10, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '-',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // 4th row
+          Row(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              // 1 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '1',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // 2 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // 3 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '3',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -272,6 +502,7 @@ class _homeState extends State<home> {
                       '+',
                       style: TextStyle(
                         color: Colors.grey[200],
+                        fontSize: button_font_size,
                       ),
                       
                       )
@@ -280,8 +511,92 @@ class _homeState extends State<home> {
               ),
             ],
           ),
-
           
+           // 5th row
+          Row(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              // 0 button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '0',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // . button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: button_width,
+
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '.',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+
+              // = button
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+                child: SizedBox(
+                  height: button_height,
+                  width: 204,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+                    ),
+
+                    child:
+                    Text(
+                      '=',
+                      style: TextStyle(
+                        color: Colors.grey[200],
+                        fontSize: button_font_size,
+                      ),
+                      
+                      )
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
 
         ],
 
