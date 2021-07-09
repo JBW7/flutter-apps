64f86a35599bccdcb6a53562b1872773de4c1c97
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-09 14:46:42 +0700

    text button

--- simple_calculator_app/lib/main.dart
+++ simple_calculator_app/lib/main.dart
@@ -20,50 +20,57 @@ class _homeState extends State<home> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(15, 50, 15, 50),
+      body: Column(
+        //crossAxisAlignment: CrossAxisAlignment.stretch,
 
-        child: Column(
-
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-
-          children: <Widget> [
-            
-            // container widget for the answer
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.blue
-              ),
-
-              child: Row(
-
-                mainAxisAlignment: MainAxisAlignment.end,
-
-                children: [
-                  Column(
-
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-                    children: [
-                      Text(
-                        '43824937',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-
+        children: <Widget>[
+          
+          Container(
+            height: 150,
+            margin: EdgeInsets.fromLTRB(10, 50, 10, 0),
+            decoration: BoxDecoration(
+              color: Colors.blue[500]
             ),
 
+            child: Row(
 
-          ],
-        ),
+              mainAxisAlignment: MainAxisAlignment.end,
 
+              children: [
+                Column(
 
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                  children: [
+                    Text(
+                      '23922342342',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+          ),
+          
+          TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
+            ),
+
+            child:
+            Text(
+              '1',
+              style: TextStyle(
+                color: Colors.grey[200],
+              ),
+              
+              )
+          )
+        ],
 
       ),
 
