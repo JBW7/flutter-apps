aba140c5dee1f73bb8b142bf29f728ff3860e9ce
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 00:01:14 +0700

    output buttons to screen

--- travel_app/lib/screens/home_screen.dart
+++ travel_app/lib/screens/home_screen.dart
@@ -15,6 +15,25 @@ class _HomeScreenState extends State<HomeScreen> {
     FontAwesomeIcons.biking,
   ];
 
+  // function to build each button
+  _buildIcon(int index) { // index used from list _icons
+    return Container( // used to control the overall shape and size of each button
+      height: 60.0,
+      width: 60.0,
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor, // color of button
+        borderRadius: BorderRadius.circular(30.0) // shape of button (radius has to be 0.5 width to be a circle)
+      ),
+      
+      child: Icon( // icon inside the button
+        _icons[index],
+        size: 25.0,
+        color: Theme.of(context).primaryColor,
+      ),
+
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +50,26 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+            
+            SizedBox(
+              height: 20.0, // give space between screen title and icons
+            ),
+
+            Row( // so that buttons is arranged in a single row
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // adds even space between each button
+              
+              children: _icons.asMap().entries.map<Widget>(
+                    (MapEntry map) => _buildIcon(map.key), // call the _buildIcon function to that all of the icons is outputed to the screen
+                  ).toList(), 
+                
+                // other way of outputing buttons onto screen however inefficient 
+                //_buildIcon(0) 
+                //_buildIcone(1)
+                //_buildIcone(1)
+                //_buildIcone(1)
+            ),
+          
           ],
         ),
       )
