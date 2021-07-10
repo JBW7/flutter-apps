49b0b2d32fa3b6258b130e9b627e4b63284e0dee
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-10 23:16:28 +0700

    list of icons

--- travel_app/lib/screens/home_screen.dart
+++ travel_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +7,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  List <IconData> _icons = [ // list of clickable icon buttons from font awesome icons flutter package
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea( // provide padding around the device so it doesnt interfere with its design
+        child: ListView( // so that user can scroll through the page
+        padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text( // screen title
+                'What Would You Like To Find?',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
   }
 }
