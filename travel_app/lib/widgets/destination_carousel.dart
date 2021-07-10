9c063b7947b784c299a36c50a9f49ba4b744dd26
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 00:33:15 +0700

    all the titles in destination carousel

---
+++ travel_app/lib/widgets/destination_carousel.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+
+class DestinationCarousel extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+              children:  <Widget> [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween, // so that the two titles is split up to each side
+
+                    children: [
+                      Text(
+                        'Top Destinations',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5,
+                        ),
+                        ),
+
+                      // See all title
+                      Text(
+                        'See All',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).primaryColor, // set color the same as the primary theme of this app
+                          letterSpacing: 1.0,
+                        ),
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+  }
+}
+
