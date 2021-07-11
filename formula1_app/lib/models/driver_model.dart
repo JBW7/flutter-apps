993b0f34beba4edfb31a9dc971ca20ed8f278d6a
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 23:34:15 +0700

    added color property

--- formula1_app/lib/models/driver_model.dart
+++ formula1_app/lib/models/driver_model.dart
@@ -3,12 +3,14 @@ class Driver {
   String rank;
   String team;
   String points;
+  //String color;
 
   Driver({
     required this.name,
     required this.rank,
     required this.team,
     required this.points,
+    //required this.color,
   });
 }
 
@@ -17,120 +19,140 @@ List <Driver> drivers = [
     name: 'Max Verstappen',
     rank: '1',
     team: 'Red Bull Racing',
-    points: '182'
+    points: '182',
+    //color: 'blue[800]'
   ),
   Driver(
     name: 'Lewis Hamilton',
     rank: '2',
     team: 'Mercedes',
-    points: '150'
+    points: '150',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Sergio Perez',
     rank: '3',
     team: 'Red Bull Racing',
-    points: '104'
+    points: '104',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Lando Norris',
     rank: '4',
     team: 'McLaren',
-    points: '101'
+    points: '101',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Valtteri Bottas',
     rank: '5',
     team: 'Mercedes',
-    points: '92'
+    points: '92',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Charles Leclerc',
     rank: '6',
     team: 'Ferrari',
-    points: '62'
+    points: '62',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Carlos Sainz',
     rank: '7',
     team: 'Ferrari',
-    points: '60'
+    points: '60',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Daniel Ricciardo',
     rank: '8',
     team: 'McLaren',
-    points: '40'
+    points: '40',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Pierre Gasly',
     rank: '9',
     team: 'AlphaTauri',
-    points: '39'
+    points: '39',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Sebastian Vettel',
     rank: '10',
     team: 'Aston Martin',
-    points: '30'
+    points: '30',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Fernando Alonso',
     rank: '11',
     team: 'Alpine',
-    points: '20'
+    points: '20',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Lance Stroll',
     rank: '12',
     team: 'Aston Martin',
-    points: '14'
+    points: '14',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Esteban Ocon',
     rank: '13',
     team: 'Alpine',
-    points: '12'
+    points: '12',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Yuki Tsunoda',
     rank: '14',
     team: 'AlphaTauri',
-    points: '9'
+    points: '9',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Kimi Räikkönen',
     rank: '15',
     team: 'Alfa Romeo Racing',
-    points: '1'
+    points: '1',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Antonio Giovinazzi',
     rank: '16',
     team: 'Alfa Romeo Racing',
-    points: '1'
+    points: '1',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'George Russel',
     rank: '17',
     team: 'Mercedes',
-    points: '0'
+    points: '0',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Mick Schumacher',
     rank: '18',
     team: 'Haas F1 Team',
-    points: '0'
+    points: '0',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Nikita Mazepin',
     rank: '19',
     team: 'Haas F1 Team',
-    points: '0'
+    points: '0',
+    //color: 'blue[800]',
   ),
   Driver(
     name: 'Nicholas Latifi',
     rank: '20',
     team: 'Williams',
-    points: '0'
+    points: '0',
+    //color: 'blue[800]',
   ),
 ];
