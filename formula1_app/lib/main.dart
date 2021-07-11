e44e4fbd0e7e61ee4a75b0a38830f7ddfa0b16c5
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 21:23:08 +0700

    driver screen

--- formula1_app/lib/main.dart
+++ formula1_app/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Formula 1',
       home: DriverScreen(),
+      // theme: ThemeData(fontFamily: 'Rajdhani'),
     );
   }
 }
