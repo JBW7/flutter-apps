6af0bef5a3ad2043011da9868dbb52bacd36b31b
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 23:12:41 +0700

    class and list of drivers and their properties

---
+++ formula1_app/lib/models/driver_model.dart
@@ -0,0 +1,136 @@
+class Driver {
+  String name;
+  String rank;
+  String team;
+  String points;
+
+  Driver({
+    required this.name,
+    required this.rank,
+    required this.team,
+    required this.points,
+  });
+}
+
+List <Driver> drivers = [
+  Driver(
+    name: 'Max Verstappen',
+    rank: '1',
+    team: 'Red Bull Racing',
+    points: '182'
+  ),
+  Driver(
+    name: 'Lewis Hamilton',
+    rank: '2',
+    team: 'Mercedes',
+    points: '150'
+  ),
+  Driver(
+    name: 'Sergio Perez',
+    rank: '3',
+    team: 'Red Bull Racing',
+    points: '104'
+  ),
+  Driver(
+    name: 'Lando Norris',
+    rank: '4',
+    team: 'McLaren',
+    points: '101'
+  ),
+  Driver(
+    name: 'Valtteri Bottas',
+    rank: '5',
+    team: 'Mercedes',
+    points: '92'
+  ),
+  Driver(
+    name: 'Charles Leclerc',
+    rank: '6',
+    team: 'Ferrari',
+    points: '62'
+  ),
+  Driver(
+    name: 'Carlos Sainz',
+    rank: '7',
+    team: 'Ferrari',
+    points: '60'
+  ),
+  Driver(
+    name: 'Daniel Ricciardo',
+    rank: '8',
+    team: 'McLaren',
+    points: '40'
+  ),
+  Driver(
+    name: 'Pierre Gasly',
+    rank: '9',
+    team: 'AlphaTauri',
+    points: '39'
+  ),
+  Driver(
+    name: 'Sebastian Vettel',
+    rank: '10',
+    team: 'Aston Martin',
+    points: '30'
+  ),
+  Driver(
+    name: 'Fernando Alonso',
+    rank: '11',
+    team: 'Alpine',
+    points: '20'
+  ),
+  Driver(
+    name: 'Lance Stroll',
+    rank: '12',
+    team: 'Aston Martin',
+    points: '14'
+  ),
+  Driver(
+    name: 'Esteban Ocon',
+    rank: '13',
+    team: 'Alpine',
+    points: '12'
+  ),
+  Driver(
+    name: 'Yuki Tsunoda',
+    rank: '14',
+    team: 'AlphaTauri',
+    points: '9'
+  ),
+  Driver(
+    name: 'Kimi Räikkönen',
+    rank: '15',
+    team: 'Alfa Romeo Racing',
+    points: '1'
+  ),
+  Driver(
+    name: 'Antonio Giovinazzi',
+    rank: '16',
+    team: 'Alfa Romeo Racing',
+    points: '1'
+  ),
+  Driver(
+    name: 'George Russel',
+    rank: '17',
+    team: 'Mercedes',
+    points: '0'
+  ),
+  Driver(
+    name: 'Mick Schumacher',
+    rank: '18',
+    team: 'Haas F1 Team',
+    points: '0'
+  ),
+  Driver(
+    name: 'Nikita Mazepin',
+    rank: '19',
+    team: 'Haas F1 Team',
+    points: '0'
+  ),
+  Driver(
+    name: 'Nicholas Latifi',
+    rank: '20',
+    team: 'Williams',
+    points: '0'
+  ),
+];
