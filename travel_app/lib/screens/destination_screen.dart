2437c5d6e876898bb544f4a862dc7ccfeaa1b35b
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-11 15:50:34 +0700

    container for image

--- travel_app/lib/screens/destination_screen.dart
+++ travel_app/lib/screens/destination_screen.dart
@@ -15,7 +15,27 @@ class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column( // so that picture of destination can be ontop of the listview
+        children: <Widget>[
+          Stack( // so that there could be text ontop of the picture
+            children: <Widget> [
+              Container(
+                height: MediaQuery.of(context).size.width, // helps to provide a layout of the screen, width meaning width of the screen
+                decoration: BoxDecoration( // rounded corners for the container widget for images
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0,
+                    )
+                  ]
+                ), 
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
