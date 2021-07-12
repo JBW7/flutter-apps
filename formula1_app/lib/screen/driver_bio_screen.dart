d67475ba6dd054a168843c4400a8c2f94b5bdc7c
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-12 13:38:07 +0700

    displayed image

--- formula1_app/lib/screen/driver_bio_screen.dart
+++ formula1_app/lib/screen/driver_bio_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 class DriverBioScreen extends StatelessWidget {
-  const DriverBioScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('hello world'),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(30),
+                  
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image(
+                    image: AssetImage('assets/images/driver_profile_bg_darken.jpg'),
+                    fit: BoxFit.cover,
+                  )
+                ),
+              ),
+              Text(driver.name)
+            ],
+          )
+        ],
       )
     );
   }
