8317359962e485c4be4898bd74dc49ac8463e19d
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-12 12:07:53 +0700

    init

---
+++ formula1_app/lib/screen/driver_bio_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DriverBioScreen extends StatelessWidget {
+  const DriverBioScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('hello world'),
+      )
+    );
+  }
+}
