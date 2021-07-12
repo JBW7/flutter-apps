635d11d5fe93bbae039221361d619cfac3bf5178
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-12 11:14:54 +0700

    pts widget

--- formula1_app/lib/widgets/drivers_list_view.dart
+++ formula1_app/lib/widgets/drivers_list_view.dart
@@ -25,46 +25,95 @@ class DriverListView extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
                   ),
-                  child: Row(
-                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15, right: 15),
-                        child: Text(
-                          driver.rank,
-                          style: TextStyle(
-                            fontSize: 22
+                  child: Container(
+                    width: 200,
+                    height: 75,
+                    child: Row(
+                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 15, right: 15),
+                          child: Text(
+                            driver.rank,
+                            style: TextStyle(
+                              fontSize: 22
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: 4,
-                        height: 50,
-                        color: Colors.blue[800],
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            driver.name,
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold
+                        Container(
+                          width: 4,
+                          height: 50,
+                          color: Colors.blue[800],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  driver.name,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                  ),
+                                Text(
+                                  driver.team,
+                                  style: TextStyle(
+                                    fontSize: 15
+                                  ),
+                                ),
+                              ],
                             ),
+                          Container(
+                            width: 70,
+                            height: 22,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey[300],
                             ),
-                          Text(
-                            driver.team,
-                            style: TextStyle(
-                              fontSize: 15
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      driver.points,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    Text(
+                                      (' PTS'),
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.blue[800],
+                              size: 16,
                             ),
                           )
-                        ],
-                      )
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
           
                 );
