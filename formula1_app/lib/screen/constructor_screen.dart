b30b7c8cf2263a42cf1ff432eb6dea9ff55a381a
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-13 12:29:20 +0700

    list widget

--- formula1_app/lib/screen/constructor_screen.dart
+++ formula1_app/lib/screen/constructor_screen.dart
@@ -1,1 +1,25 @@
-// display constructors standings
+import 'package:flutter/material.dart';
+
+class ConstructorScreen extends StatefulWidget {
+
+  @override
+  _ConstructorScreenState createState() => _ConstructorScreenState();
+}
+
+class _ConstructorScreenState extends State<ConstructorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Constructors Standings'),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            ConstructorListView()
+          ],
+        ),
+      ),
+    );
+  }
+}
