38a1f0568a36b297dd3c049534c55e296a74a634
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-13 21:01:15 +0700

    stack widgets

--- formula1_app/lib/screen/constructor_bio_screen.dart
+++ formula1_app/lib/screen/constructor_bio_screen.dart
@@ -15,61 +15,93 @@ class _ConstructorBioScreenState extends State<ConstructorBioScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: 250,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(30)
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: Image(
-                image: AssetImage('assets/images/driver_profile_bg_darken.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  iconSize: 30,
-                  color: Colors.white,
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                )
-              ],
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(left: 20, top: 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 1500,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey[850],
+              child: Stack(
                 children: [
-                  Row(
-                    children: [
-                      Container(
-                        height: 80,
-                        width: 4,
-                        decoration: BoxDecoration(
-                          color: Colors.blue[800]
+                  Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(30)
+                    
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image(
+                        image: AssetImage('assets/images/driver_profile_bg_darken.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30,
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 30, left: 20),
+                      child: Container(
+                        height: 88,
+                        width: 400,
+                        //color: Colors.white,
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 88,
+                                width: 4,
+                                color: Colors.blue[800],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Row(
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          widget.constructor.name,
+                                          style: TextStyle(
+                                            fontSize: 32,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white
+                                          ),
+                                        ),
+                                        Image(
+                                          image: AssetImage(widget.constructor.logoUrl),
+                                          height: 50,
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                      Padding()
-                    ],
+                    ),
                   )
-                ],
+                ], 
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
