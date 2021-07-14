c60e35dd97ae874affafc48d7eb914104a6d7b2d
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-14 17:15:53 +0700

    race class

---
+++ formula1_app/lib/models/race_model.dart
@@ -0,0 +1,33 @@
+class Race {
+  String round;
+  String date;
+  String month;
+  String background;
+  String location;
+  String venue;
+  String flag;
+  String track;
+  String circuitLength;
+  String noLaps;
+  String firstGp;
+  String raceDistance;
+  String lapRecordTime;
+  String lapRecordHolderYear;
+
+  Race({
+    required this.round,
+    required this.date,
+    required this.month,
+    required this.background,
+    required this.location,
+    required this.venue,
+    required this.flag,
+    required this.track,
+    required this.circuitLength,
+    required this.noLaps,
+    required this.firstGp,
+    required this.raceDistance,
+    required this.lapRecordTime,
+    required this.lapRecordHolderYear,
+  });
+}
