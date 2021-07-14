7c5b1772438cd73c375b394485eb02d139cc8daa
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-14 21:54:41 +0700

    added achievement  widgets

--- formula1_app/lib/screen/constructor_bio_screen.dart
+++ formula1_app/lib/screen/constructor_bio_screen.dart
@@ -21,93 +21,160 @@ class _ConstructorBioScreenState extends State<ConstructorBioScreen> {
             Container(
               height: 1500,
               width: MediaQuery.of(context).size.width,
-              color: Colors.grey[850],
-              child: Stack(
+              color: Colors.white,
+              child: Column(
                 children: [
-                  Container(
-                    height: 250,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(30)
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Image(
-                        image: AssetImage('assets/images/driver_profile_bg_darken.jpg'),
-                        fit: BoxFit.cover,
+                  Stack(
+                    children: [
+                      Container(
+                        height: 250,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30)
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image(
+                            image: AssetImage('assets/images/driver_profile_bg_darken.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          iconSize: 30,
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                      SafeArea(
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 30, left: 20),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 88,
+                                width: 400,
+                                //color: Colors.white,
+                                child: Container(
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 88,
+                                        width: 4,
+                                        color: Colors.blue[800],
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 10),
+                                        child: Row(
+                                          children: [
+                                            Column(
+                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  widget.constructor.name,
+                                                  style: TextStyle(
+                                                    fontSize: 32,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white
+                                                  ),
+                                                ),
+                                                Image(
+                                                  image: AssetImage(widget.constructor.logoUrl),
+                                                  height: 50,
+                                                )
+                                              ],
+                                            ),
+                                          ],
+                                        )
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 125.0),
+                                child: Image(
+                                  image: AssetImage(widget.constructor.carUrl),
+                                  height: 75,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ], 
                   ),
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      iconSize: 30,
-                      color: Colors.white,
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ),
-                  SafeArea(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 30, left: 20),
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 88,
-                            width: 400,
-                            //color: Colors.white,
-                            child: Container(
-                              child: Row(
-                                children: [
-                                  Container(
-                                    height: 88,
-                                    width: 4,
-                                    color: Colors.blue[800],
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(left: 10),
-                                    child: Row(
-                                      children: [
-                                        Column(
-                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              widget.constructor.name,
-                                              style: TextStyle(
-                                                fontSize: 32,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.white
-                                              ),
-                                            ),
-                                            Image(
-                                              image: AssetImage(widget.constructor.logoUrl),
-                                              height: 50,
-                                            )
-                                          ],
-                                        ),
-                                      ],
-                                    )
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 125.0),
-                            child: Image(
-                              image: AssetImage(widget.constructor.carUrl),
-                              height: 75,
-                            ),
-                          )
-                        ],
+                    padding: EdgeInsets.only(top: 20, bottom: 20),
+                    child: Text(
+                      'Achievements',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold
                       ),
                     ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Container(
+                          height: 320,
+                          width: 228,
+                          color: Colors.blue,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Victories',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Pole Positions',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Fastest Laps',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Constructor Championships',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 80),
+                        child: Container(
+                          height: 320,
+                          width: 100,
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
                   )
-                ], 
+                ],
               ),
             ),
           ],
