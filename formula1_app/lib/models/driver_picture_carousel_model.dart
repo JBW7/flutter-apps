323eb809519c48ddef3da609e22d9a66127cb2c7
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-14 14:43:10 +0700

    init

---
+++ formula1_app/lib/models/driver_picture_carousel_model.dart
@@ -0,0 +1,17 @@
+class PictureCarousel {
+  String image1url;
+  String image2url;
+  String image3url;
+  String image4url;
+  String image5url;
+  String image6url;
+
+  PictureCarousel({
+    required this.image1url,
+    required this.image2url,
+    required this.image3url,
+    required this.image4url,
+    required this.image5url,
+    required this.image6url,
+  });
+}
