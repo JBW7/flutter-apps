f4dea102892ac3a2f8d48f8420c6281627aeb54d
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-15 18:29:43 +0700

    init

---
+++ formula1_app/lib/screen/race_info_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:formula1_app/models/race_model.dart';
+
+class RaceInfoScreen extends StatefulWidget {
+  final Race race;
+
+  RaceInfoScreen({required this.race});
+
+  @override
+  _RaceInfoScreenState createState() => _RaceInfoScreenState();
+}
+
+class _RaceInfoScreenState extends State<RaceInfoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
