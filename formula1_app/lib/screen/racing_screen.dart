9c59d905a1fcaebc291acf295d3f9a47e8b37f8a
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-15 17:37:00 +0700

    appbar

--- formula1_app/lib/screen/racing_screen.dart
+++ formula1_app/lib/screen/racing_screen.dart
@@ -1,1 +1,23 @@
-// display races upcoming and past
+import 'package:flutter/material.dart';
+import 'package:formula1_app/widgets/racing_list_view.dart';
+
+class RacingScreen extends StatefulWidget {
+
+  @override
+  _RacingScreenState createState() => _RacingScreenState();
+}
+
+class _RacingScreenState extends State<RacingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Racing'),
+        backgroundColor: Colors.grey[850],
+      ),
+      body: SafeArea(
+        child: RacingListView(),
+      ),
+    );
+  }
+}
