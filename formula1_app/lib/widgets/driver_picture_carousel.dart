5e4317a141e6acc118c8d4ff09a1b87d29a716a2
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-15 22:55:43 +0700

    driver property

--- formula1_app/lib/widgets/driver_picture_carousel.dart
+++ formula1_app/lib/widgets/driver_picture_carousel.dart
@@ -4,33 +4,40 @@ import 'package:formula1_app/models/driver_picture_carousel_model.dart';
 
 class DriverPictureCarousel extends StatelessWidget {
 
+  // final Driver driver;
+
+  // DriverPictureCarousel({required this.driver});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 230,
-      color: Colors.white,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: pictureCarousels.length,
-        itemBuilder: (BuildContext context, int index) {
-          PictureCarousel pictureCarousel = pictureCarousels[index];
-          return Container(
-            margin: EdgeInsets.fromLTRB(8, 8, 0, 8),
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image(
-                image: AssetImage(pictureCarousel.maxVerstappen),
-                fit: BoxFit.cover,
-              ),
-            ),
-          );
-        }
-      ),
+    return Column(
+      children: [
+        Container(
+          height: 230,
+          color: Colors.white,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: pictureCarousels.length,
+            itemBuilder: (BuildContext context, int index) {
+              PictureCarousel pictureCarousel = pictureCarousels[index];
+              return Container(
+                margin: EdgeInsets.fromLTRB(8, 8, 0, 8),
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image(
+                    image: AssetImage(pictureCarousel.maxVerstappen),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            }
+          ),
+        ),
+      ],
     );
   }
 }
