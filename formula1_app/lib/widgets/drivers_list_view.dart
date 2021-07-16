429486fa7a93ffbaaa991a10e1fd3c63b4f0c7cc
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-16 21:04:34 +0700

    font

--- formula1_app/lib/widgets/drivers_list_view.dart
+++ formula1_app/lib/widgets/drivers_list_view.dart
@@ -6,122 +6,124 @@ class DriverListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 0),
-      child: Container(
-        height: 730,
-        color: Colors.grey[850],
-        child: ListView.builder(
-          itemCount: drivers.length,
-          itemBuilder: (BuildContext context, int index) {
-            Driver driver = drivers[index];
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => DriverBioScreen(driver: driver,))
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                width: 320,
-                height: 75,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 20),
-                          child: Container(
-                            color: Colors.white,
-                            width: 28,
-                            child: Text(
-                              driver.rank,
-                              style: TextStyle(
-                                fontSize: 22,
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(top: 0),
+        child: Container(
+          height: 730,
+          color: Colors.grey[850],
+          child: ListView.builder(
+            itemCount: drivers.length,
+            itemBuilder: (BuildContext context, int index) {
+              Driver driver = drivers[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => DriverBioScreen(driver: driver,))
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                  width: 320,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Container(
+                              color: Colors.white,
+                              width: 28,
+                              child: Text(
+                                driver.rank,
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 5),
-                          child: Container(
-                            width: 4,
-                            height: 50,
-                            color: driver.color,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              driver.name,
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold
-                              ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 5),
+                            child: Container(
+                              width: 4,
+                              height: 50,
+                              color: driver.color,
                             ),
-                            Text(
-                              driver.team,
-                              style: TextStyle(fontSize: 15),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 70,
-                          height: 22,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(10)
                           ),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
                             mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                driver.points,
+                                driver.name,
                                 style: TextStyle(
-                                  fontSize: 16,
+                                  fontSize: 24,
                                   fontWeight: FontWeight.bold
                                 ),
                               ),
                               Text(
-                                'PTS',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                ),
-                              ),
+                                driver.team,
+                                style: TextStyle(fontSize: 15),
+                              )
                             ],
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            width: 70,
+                            height: 22,
+                            decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  driver.points,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Text(
+                                  'PTS',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 4, right: 8),
-                          child: Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.red,
-                            size: 16,
+                          Padding(
+                            padding: EdgeInsets.only(left: 4, right: 8),
+                            child: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.red,
+                              size: 16,
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            );
-          }
+              );
+            }
+          ),
         ),
       ),
     );
