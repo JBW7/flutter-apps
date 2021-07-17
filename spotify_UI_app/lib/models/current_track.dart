3535a93281fbf6ac7e0fbd416fbb4fc6dd1a1e07
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-18 00:33:14 +0700

    notify listeners

--- spotify_UI_app/lib/models/current_track.dart
+++ spotify_UI_app/lib/models/current_track.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:spotify_UI_app/data/data.dart';
 
 class CurrentTrackModel extends ChangeNotifier {
-  Song?
+  Song? selected;
+
+  void selectTrack(Song track) {
+    selected = track;
+    notifyListeners();
+  }
 }
