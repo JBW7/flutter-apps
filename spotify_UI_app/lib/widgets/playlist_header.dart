c5bb3a0e9ecc05711712a969f1f94d8038cd6673
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-17 23:11:29 +0700

    name and description

--- spotify_UI_app/lib/widgets/playlist_header.dart
+++ spotify_UI_app/lib/widgets/playlist_header.dart
@@ -3,15 +3,55 @@ import 'package:spotify_UI_app/data/data.dart';
 
 class PlaylistHeader extends StatelessWidget {
 
-  final Playlist playlist;/
-
-
+  final Playlist playlist;
 
   const PlaylistHeader({ Key? key , required this.playlist}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Row(
+      children: [
+        Image(
+          image: AssetImage(
+            playlist.imageURL,
+          ),
+          height: 200,
+          width: 200,
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Playlist',
+                style: Theme.of(context)
+                .textTheme.overline!
+                .copyWith(
+                  fontSize: 12
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Text(
+                playlist.name,
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Text(
+                playlist.description,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
