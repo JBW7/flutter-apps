0eaac46514ea995416d0716c48c75a882e77231e
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-17 18:26:28 +0700

    side menu items

--- spotify_UI_app/lib/widgets/side_menu.dart
+++ spotify_UI_app/lib/widgets/side_menu.dart
@@ -10,13 +10,68 @@ class SideMenu extends StatelessWidget {
       color: Theme.of(context).primaryColor,
       child: Column(
         children: [
-          Image.asset(
-            'assets/spotify_logo.png',
-            height: 55,
-            filterQuality: FilterQuality.high,
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/spotify_logo.png',
+                  height: 55,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
+          SideMenuIconTab(
+            iconData: Icons.home,
+            title: 'Home',
+            onTap: () {}
+          ),
+          SideMenuIconTab(
+            iconData: Icons.search,
+            title: 'Search',
+            onTap: () {}
+          ),
+          SideMenuIconTab(
+            iconData: Icons.audiotrack,
+            title: 'Radio',
+            onTap: () {}
           ),
         ],
       ),
+    );
+  }
+}
+
+class SideMenuIconTab extends StatelessWidget {
+
+  final IconData iconData;
+  final String title;
+  final VoidCallback onTap;
+
+  const SideMenuIconTab({ 
+    Key? key,
+
+    required this.iconData,
+    required this.title,
+    required this.onTap,
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        iconData,
+        color: Theme.of(context).iconTheme.color,
+        size: 28,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: onTap,
     );
   }
 }
