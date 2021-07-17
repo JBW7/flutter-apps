14a48e2a4eca93a4232923cecc218ae454f9d5bf
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-17 23:42:53 +0700

    play button

--- spotify_UI_app/lib/widgets/playlist_header.dart
+++ spotify_UI_app/lib/widgets/playlist_header.dart
@@ -9,57 +9,98 @@ class PlaylistHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Image(
-          image: AssetImage(
-            playlist.imageURL,
-          ),
-          height: 200,
-          width: 200,
-          fit: BoxFit.cover,
+        Row(
+          children: [
+            Image(
+              image: AssetImage(
+                playlist.imageURL,
+              ),
+              height: 200,
+              width: 200,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Playlist',
+                    style: Theme.of(context)
+                    .textTheme.overline!
+                    .copyWith(
+                      fontSize: 12
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    playlist.name,
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    playlist.description,
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Created by ${playlist.creator} • ${playlist.songs.length} songs, ${playlist.duration}',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
         const SizedBox(
-          width: 16,
+          height: 20,
         ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Playlist',
-                style: Theme.of(context)
-                .textTheme.overline!
-                .copyWith(
-                  fontSize: 12
-                ),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text(
-                playlist.name,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                playlist.description,
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Created by ${playlist.creator} • ${playlist.songs.length} songs, ${playlist.duration}',
-                style: Theme.of(context).textTheme.subtitle1,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              _PlaylistButtons(followers: playlist.followers)
-            ],
+        _PlaylistButtons(followers: playlist.followers)
+      ],
+    );
+  }
+}
+
+class _PlaylistButtons extends StatelessWidget {
+
+  final String followers;
+
+  const _PlaylistButtons({ Key? key, required this.followers}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 48, vertical: 20
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
+            ),
+            backgroundColor: Theme.of(context).accentColor,
+            primary: Theme.of(context).iconTheme.color,
+            textStyle: Theme.of(context)
+            .textTheme.caption!
+            .copyWith(
+              fontSize: 12, 
+              letterSpacing: 2
+            ),
+          ),
+          child: const Text(
+            'Play'
           ),
         )
       ],
