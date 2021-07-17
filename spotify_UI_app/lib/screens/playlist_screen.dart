403bc1ddd5d1715006de7428dcc7d86a204fe559
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-17 20:40:37 +0700

    gradient background

--- spotify_UI_app/lib/screens/playlist_screen.dart
+++ spotify_UI_app/lib/screens/playlist_screen.dart
@@ -15,8 +15,25 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              const Color(0xFFAF1018),
+              Theme.of(context).backgroundColor
+            ],
+            stops: [0, 0.3]
+          )
+        ),
+      ),
     );
   }
 }
