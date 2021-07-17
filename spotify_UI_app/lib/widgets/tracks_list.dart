b11217fa23058629ddf6f410cf7d1e13d19ae0dd
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-18 00:21:06 +0700

    complete tracklist

--- spotify_UI_app/lib/widgets/tracks_list.dart
+++ spotify_UI_app/lib/widgets/tracks_list.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_UI_app/data/data.dart';
 
 class TrackList extends StatelessWidget {
-  const TrackList({ Key? key }) : super(key: key);
+  
+  final List<Song> tracks;
+
+  const TrackList({ Key? key, required this.tracks}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return DataTable(
+      columns: const [
+        DataColumn(
+          label: Text('Title')
+        ),
+        DataColumn(
+          label: Text('Artist')
+        ),
+        DataColumn(
+          label: Text('Album')
+        ),
+        DataColumn(
+          label: Icon(Icons.access_time)
+        ),
+      ],
+      rows: tracks.map((e) { // e is the song
+        return DataRow(
+          cells: [
+            DataCell(
+              Text(
+                e.title,
+                style: TextStyle(
+                  color: Theme.of(context).iconTheme.color,
+                )
+              )
+            ),
+            DataCell(
+              Text(
+                e.artist,
+                style: TextStyle(
+                  color: Theme.of(context).iconTheme.color,
+                )
+              )
+            ),
+            DataCell(
+              Text(
+                e.album,
+                style: TextStyle(
+                  color: Theme.of(context).iconTheme.color,
+                )
+              )
+            ),
+            DataCell(
+              Text(
+                e.duration,
+                style: TextStyle(
+                  color: Theme.of(context).iconTheme.color,
+                )
+              )
+            )
+          ]
+        );
+      }).toList()
     );
   }
 }
