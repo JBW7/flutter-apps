d581faa3ba153e199bf486ab962647f6c6f1b966
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-18 12:11:34 +0700

    track info

--- spotify_UI_app/lib/widgets/current_track.dart
+++ spotify_UI_app/lib/widgets/current_track.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_UI_app/models/current_track.dart';
+import 'package:provider/provider.dart';
 
 class CurrentTrack extends StatelessWidget {
   const CurrentTrack({ Key? key }) : super(key: key);
@@ -8,7 +10,7 @@ class CurrentTrack extends StatelessWidget {
     return Container(
       height: 84,
       width: double.infinity,
-      color: Colors.blue,
+      color: Colors.black87,
       child: Padding(
         padding: EdgeInsets.all(12),
         child: Row(
@@ -26,8 +28,40 @@ class _TrackInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final selected = context.watch<CurrentTrackModel>().selected;
+    if (selected == null) return const SizedBox.shrink();
+    return Row(
+      children: [
+        Image(
+          image: AssetImage('assets/lofigirl.jpg'),
+          height: 60,
+          width: 60,
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(
+          width: 12,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              selected.title,
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              selected.artist,
+              style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                color: Colors.grey[300],
+                fontSize: 12,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
