01a3955f608bff662da40c450db5bd1cc52ec688
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-18 11:54:48 +0700

    track info widget

--- spotify_UI_app/lib/widgets/current_track.dart
+++ spotify_UI_app/lib/widgets/current_track.dart
@@ -9,6 +9,25 @@ class CurrentTrack extends StatelessWidget {
       height: 84,
       width: double.infinity,
       color: Colors.blue,
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Row(
+          children: [
+            _TrackInfo()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TrackInfo extends StatelessWidget {
+  const _TrackInfo({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
