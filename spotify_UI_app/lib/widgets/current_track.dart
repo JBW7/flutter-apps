e415d33132db998910e1e9d578c70744b4d45d49
Jonathan150305 <jonathanbernard265@gmail.com>
2021-07-18 11:53:06 +0700

    stless

---
+++ spotify_UI_app/lib/widgets/current_track.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurrentTrack extends StatelessWidget {
+  const CurrentTrack({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 84,
+      width: double.infinity,
+      color: Colors.blue,
+    );
+  }
+}
